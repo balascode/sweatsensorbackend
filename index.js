5fbe492b67a32b2d02cca46f232eae5d52ffada4
balascode <163442336+balascode@users.noreply.github.com>
2025-03-29 01:31:30 +0530

    deployment issue

--- index.js
+++ index.js
@@ -4,25 +4,16 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://sweatsensor.vercel.app",
-];
-
 const io = new Server(server, {
   cors: {
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      "http://localhost:3000",        
+      "https://sweatsensor.vercel.app",
+    ],
     methods: ["GET", "POST"],
-    credentials: true,
+    credentials: true, 
   },
 });
-
 
 
 
@@ -49,4 +40,5 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => clearInterval(interval));
 });
 
-server.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
