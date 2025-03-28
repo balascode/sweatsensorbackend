a619274f0056596687e1a7f5a6a98146b4cbcee0
balascode <163442336+balascode@users.noreply.github.com>
2025-03-29 01:12:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "https://sweatsensor.vercel.app/" } });
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:3000",        
+      "https://sweatsensor.vercel.app",
+    ],
+    methods: ["GET", "POST"],
+    credentials: true, 
+  },
+});
+
+
 
 // Simulate sweat data 
 function generateSweatData() {
